ec9e32fbe7476ba9bc556ce5e104530a091e19ac
Winzar <connorwinzar@gmail.com>
2022-04-07 15:37:05 +0100

    Logic change: class

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,8 +1,34 @@
 "use strict";
 
-var card = document.querySelector(".front-face");
-console.log(card);
-card.addEventListener('click', function () {
-  console.log("I work");
-  card.style.visibility = "hidden";
-});
+// const card = document.querySelectorAll(".flip-card") ;
+var card = document.querySelector(".card-number-1");
+var cardInner = document.querySelector(".flip-card-inner"); // console.log(card)
+
+var flippedStatus = false; //for each
+//Function that removes the card from display + no longer interactable
+
+card.addEventListener('click', function (event) {
+  console.log(event.currentTarget); //card.style.visibility = "hidden";
+
+  if (flippedStatus == false) {
+    cardInner.classList.toggle("card-number-1--flipped"); //cardInner.style.cssText = "transform: rotateY(180deg)";
+
+    console.log("flippedStatus has been recognised as true");
+    flippedStatus = true;
+  } else if (flippedStatus == true) {
+    //cardInner.style.transform= null; 
+    card.classList.toggle("card-number-1--flipped");
+    console.log("flippedStatus has been recognised as false");
+    flippedStatus = false;
+  }
+}); // cardInner.style.transform= null; 
+// Function that removes the card from display + no longer interactable
+// card.addEventListener('click', () => {
+//     console.log("I work")
+//     card.style.visibility = "hidden";
+// });
+// setTimeout(() => {
+//     console.log("command removed")
+//     //cardInner.style.removeProperty = "transform";   
+//     cardInner.style.cssText = "transform: rotateY(180deg)"; 
+// }, 2000);
--- main.js
+++ main.js
@@ -1,10 +1,48 @@
-const card = document.querySelector(".front-face")
+// const card = document.querySelectorAll(".flip-card") ;
+const card = document.querySelector(".card-number-1") ;
+const cardInner = document.querySelector(".flip-card-inner");
 
-console.log(card)
 
-card.addEventListener('click', () => {
-    console.log("I work")
-    card.style.visibility = "hidden";
 
+
+// console.log(card)
+
+
+var flippedStatus= false
+
+//for each
+
+//Function that removes the card from display + no longer interactable
+card.addEventListener('click', (event) => {
+    console.log(event.currentTarget)
+    //card.style.visibility = "hidden";
+    if (flippedStatus == false) {
+        cardInner.classList.toggle("card-number-1--flipped")
+        //cardInner.style.cssText = "transform: rotateY(180deg)";
+        console.log("flippedStatus has been recognised as true");
+        flippedStatus= true;
+    } else if (flippedStatus == true) {
+        //cardInner.style.transform= null; 
+        card.classList.toggle("card-number-1--flipped");
+        console.log("flippedStatus has been recognised as false");
+        flippedStatus= false;
+    } 
 });
 
+
+
+// cardInner.style.transform= null; 
+
+
+// Function that removes the card from display + no longer interactable
+// card.addEventListener('click', () => {
+//     console.log("I work")
+//     card.style.visibility = "hidden";
+
+// });
+
+// setTimeout(() => {
+//     console.log("command removed")
+//     //cardInner.style.removeProperty = "transform";   
+//     cardInner.style.cssText = "transform: rotateY(180deg)"; 
+// }, 2000);
